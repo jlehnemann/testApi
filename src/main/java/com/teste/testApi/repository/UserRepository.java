54d86f999fdd67da523ee22e1bf6029a6a3db790
Jose <j.lehnemann@gmail.com>
2026-04-22 21:40:56 -0300

    First commit

---
+++ src/main/java/com/teste/testApi/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.teste.testApi.repository;
+
+import com.teste.testApi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
